283e124dcce1e4ff16e5395871fa54dc23ad05f9
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-09 14:44:40 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  
+
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessionss#create"
+  get "/logout" => "sessions#destroy"
+ 
+
+end
